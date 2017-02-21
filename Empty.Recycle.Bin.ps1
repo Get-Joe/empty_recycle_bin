e6b1d6da9dc6f98b9b748995174bfdf84e6f6443
Get-Joe <sithlord1977@gmail.com>
2017-02-21 10:48:01 -0500

    First Draft (Untested)

--- Empty.Recycle.Bin.ps1
+++ Empty.Recycle.Bin.ps1
@@ -3,3 +3,16 @@
 # It is created for an enterprise environment.
 # Further information can be found in the README file.
 
+$objUser = New-Object System.Security.Principal.NTAccount("$env:UserName")
+$strSID = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+$strSID.Value | ForEach-Object ($_)
+{
+    $RemoveRecycleBin = '\$Recycle.Bin\' + $_
+}
+ForEach ($Drive in Get-PSDrive -PSProvider FileSystem)
+{
+    $Path = $Drive.Name + $RemoveRecycleBin
+    Get-ChildItem $Path -Force -Recurse -ErrorAction SilentlyContinue |
+    Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-30)} |
+    Remove-Item -Recurse -Force
+}
