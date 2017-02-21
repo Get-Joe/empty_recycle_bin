c2e11d8142fb98d812a08ba15c645a0f05c4efc2
Get-Joe <sithlord1977@gmail.com>
2017-02-21 10:18:21 -0500

    Begining of notes.

---
+++ Empty.Recycle.Bin.ps1
@@ -0,0 +1,5 @@
+# A Scrypt by Joe.
+# The following is a scrypt to remove items from the recycle bin based on the last time they were accessed.
+# It is created for an enterprise environment.
+# Further information can be found in the README file.
+
